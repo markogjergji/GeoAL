a0e999c38a0e0782deb1e3cd01b25cc2990f0b97
marko_gjergji <55448578+markogjergji@users.noreply.github.com>
2021-02-03 23:26:25 +0100

    Update util.py

--- geo/util.py
+++ geo/util.py
@@ -7,7 +7,7 @@ from django.contrib.staticfiles import finders
 
 
 def checkCoordinates():
-    file_path = finders.find('geo/validPoints.txt')
+    file_path = finders.find('validPoints.txt')
     g = open(file_path, "r")
     i = 0
     points = []
