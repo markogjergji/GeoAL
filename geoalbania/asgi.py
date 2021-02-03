a40d0830cabc980d5ff99e221f3965ed8f5816ff
srgdn <marko39993@gmail.com>
2021-02-03 21:14:28 +0100

    Singleplayer_first_version

--- geoalbania/asgi.py
+++ geoalbania/asgi.py
@@ -6,15 +6,10 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
-
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geoalbania.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{ project_name }}.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
-})
+application = get_asgi_application()
